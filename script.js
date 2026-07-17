ee16dd29e5f83bc10e6960d076c3868aa469260b
Kim Carl Macapayad <kimcarlmac@gmail.com>
2026-07-17 19:54:46 +0800

    Initial Commit (nav bar and hero)

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mouse Spotlight
+const glow = document.createElement("div");
+glow.className = "cursor-glow";
+document.body.appendChild(glow);
+ 
+document.addEventListener("mousemove", (e) => {
+  glow.style.left = `${e.clientX}px`;
+  glow.style.top = `${e.clientY}px`;
+});
