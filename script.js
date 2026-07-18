a11deedd00ff1314360e6247518ddafd978bf192
Kim Carl Macapayad <kimcarlmac@gmail.com>
2026-07-18 13:11:03 +0800

    github contribution heatmap script

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ document.addEventListener("mousemove", (e) => {
   glow.style.left = `${e.clientX}px`;
   glow.style.top = `${e.clientY}px`;
 });
+
+// GitHub ontribution heatmap squares
+const heatmapGrid = document.getElementById("heatmapGrid");
+const totalDays = 371; 
+ 
+for (let i = 0; i < totalDays; i++) {
+  const square = document.createElement("div");
+ 
+  // random intensity level from 0 (no activity) to 4 (very active)
+  const level = Math.floor(Math.random() * 5);
+  square.className = `heatmap-square level-${level}`;
+ 
+  heatmapGrid.appendChild(square);
+}
